be2661d118c81e55bbcaa787731aeb4a0be28567
B0218986 <b0218986@LTB0218986-Mac.local>
2020-07-06 18:22:10 +0530

    articles loading

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles : [],
+      key : '',
+    };
+  }
+
+  componentDidMount() {
+    //const key = 'd0c0f9ae66bf4ec69c4a71c94c2cc0f1';
+    const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=d0c0f9ae66bf4ec69c4a71c94c2cc0f1';
+    var req = new Request(url);
+
+    fetch(url).then(
+      response => response.json()
+    ).then(
+      (data) => {
+        this.setState(
+          {articles : data.articles}
+        );
+        //console.log(this.state.articles);
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.articles.map((item, index) => {
+          return (
+            <div>
+              <div>
+                <img src={item.urlToImage} width="300"/>
+              </div>
+              <div>
+                {item.title}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
