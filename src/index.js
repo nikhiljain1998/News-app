cbb4420a9268982fb2cc3994c1d4dbd3caa04ce3
B0218986 <b0218986@LTB0218986-Mac.local>
2020-07-06 15:36:49 +0530

    initial push

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function API() {
+  var url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=d0c0f9ae66bf4ec69c4a71c94c2cc0f1';
+  var req = new Request(url);
+  fetch(req).then(
+    function(response) {
+      console.log(response.json());
+    }
+  );
+  return null;
+}
+
 ReactDOM.render(
   <React.StrictMode>
+    <API />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
