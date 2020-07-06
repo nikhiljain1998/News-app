45a949d221f4b865260e7179677cfabd099cdaf1
B0218986 <b0218986@LTB0218986-Mac.local>
2020-07-06 19:17:07 +0530

    search working

--- src/App.js
+++ src/App.js
@@ -8,13 +8,20 @@ class App extends React.Component {
     this.state = {
       articles : [],
       key : '',
+      searchValue : '',
     };
   }
 
+  updateSearchValue(props) {
+    this.setState(
+      {searchValue : props.target.value}
+    );
+  }
+
   componentDidMount() {
-    //const key = 'd0c0f9ae66bf4ec69c4a71c94c2cc0f1';
-    const url = 'http://newsapi.org/v2/top-headlines?country=us&apiKey=d0c0f9ae66bf4ec69c4a71c94c2cc0f1';
-    var req = new Request(url);
+    const key = 'd0c0f9ae66bf4ec69c4a71c94c2cc0f1';
+    const url = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=' + key;
+    //var req = new Request(url);
 
     fetch(url).then(
       response => response.json()
@@ -28,9 +35,31 @@ class App extends React.Component {
     );
   }
 
+  handleClick (props) {
+    const url = 'http://newsapi.org/v2/everything?' +
+      'q=' + props +
+      '&from=2020-07-06&' +
+      'sortBy=popularity&' +
+      'apiKey=d0c0f9ae66bf4ec69c4a71c94c2cc0f1';
+    //var req = new Request(url);
+
+    fetch(url).then(
+      (response) => response.json()
+    ).then(
+      (data) => {
+        this.setState(
+          {articles : data.articles}
+        );
+        //console.log(this.state.articles);
+      }
+    );
+  }
+
   render() {
     return (
       <div>
+        <input type='text' placeholder='Search' value={this.state.searchValue} onChange={(val) => this.updateSearchValue(val)}></input>
+        <button onClick={ () => this.handleClick(this.state.searchValue)}>Go!</button>
         {this.state.articles.map((item, index) => {
           return (
             <div>
